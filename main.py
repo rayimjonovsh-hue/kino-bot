d5535313baab820c29da96faf1f309b50e44deb7
shamshod <rayimjonovsh@gmail.com>
2026-09-20 10:49:31 +0200

    uptade 1

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ async def start_web():
 async def main():
     logging.basicConfig(level=logging.INFO)
     await start_web()
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
